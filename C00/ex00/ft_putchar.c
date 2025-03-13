ec4073d180461ae970cdc979c521336cc2fea1bc
Alexandre Broslavetz Spader <abroslav@c1r11s8.42porto.com>
2025-03-13 15:49:00 +0000

    C00 - ex00

---
+++ C00/ex00/ft_putchar.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abroslav <abroslav@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 21:40:07 by abroslav          #+#    #+#             */
+/*   Updated: 2025/03/13 15:44:08 by abroslav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+/*int	main(void)
+{
+	ft_putchar('c');
+	return (0);
+}*/
+
+//write a function that display the character passed as a parameter.
