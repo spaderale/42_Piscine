3c83e633f3b4a221535ba17b629595395bfd03d9
Alexandre Broslavetz Spader <abroslav@c1r11s8.42porto.com>
2025-03-13 16:19:02 +0000

    C01 ex00

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abroslav <abroslav@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 12:12:31 by abroslav          #+#    #+#             */
+/*   Updated: 2025/03/13 16:18:50 by abroslav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+/*int	main(void)
+{
+	int	n;
+
+	n = 0;
+	ft_ft(&n);
+	printf("O valor de n é: %d", n);
+	return (0);
+}*/
+
+//Create a function that takes a pointer to int as a parameter, and sets the value "42"
+//to that int.
