dd1174b49ca83ae5e036275d9265ffb7f40b9486
Alexandre Broslavetz Spader <abroslav@c1r11s8.42porto.com>
2025-03-13 16:26:03 +0000

    C01 ex05

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abroslav <abroslav@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 13:45:26 by abroslav          #+#    #+#             */
+/*   Updated: 2025/03/13 16:25:57 by abroslav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+/*int	main(void)
+{
+	char frase[15] = "Keep swimming!";
+	ft_putstr(frase);
+	return(0);
+}*/
+
+//Create a function that displays a string of characters on the standard output
