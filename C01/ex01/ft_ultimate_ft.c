fce52cb9f9a9d439c33d30184fed9b222400df63
Alexandre Broslavetz Spader <abroslav@c1r11s8.42porto.com>
2025-03-13 16:20:03 +0000

    C01 ex01

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abroslav <abroslav@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 12:26:31 by abroslav          #+#    #+#             */
+/*   Updated: 2025/03/13 16:19:53 by abroslav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int n = 0;
+	int *n1 = &n;
+	int **n2 = &n1;
+	int ***n3 = &n2;
+	int ****n4 = &n3;
+	int *****n5 = &n4;
+	int ******n6 = &n5;
+	int *******n7 = &n6;
+	int ********n8 = &n7;
+	int *********n9 = &n8;
+	
+	printf("valor de n: %d \n", n);
+	ft_ultimate_ft(n9);
+	printf("valor de n: %d", n);
+	return(0);
+}*/
+
+//Create a function that takes a pointer to pointer to pointer to pointer to pointer
+//to pointer to pointer to pointer to pointer to int as a parameter and sets the value
+//"42" to that int.
